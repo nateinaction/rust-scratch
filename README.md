b2411ed43556755fd30351179f88a13b75fd2b14
Nate Gay <email@nategay.me>
2019-06-15 22:08:26 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # rust-scratch
+
+Following along with the [Rust book](https://doc.rust-lang.org/book/)
+
+### Sections:
+#### 1 Getting Started
+- 1.2 [Hello, World!](hello_world/)
+- 1.3 [Hello, Cargo!](hello_cargo/)
+
+#### 2 Programming a Guessing Game
+- [Exercise](guessing_game/)
+
+#### 3 Common Programming Concepts
---
+++ guessing_game/src/main.rs
@@ -0,0 +1,36 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("Guess the number!");
+
+    loop {
+        // vars are immutable by default, we must declare mutability
+        let mut guess = String::new();
+        println!("Please input your guess.");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // How to annotate type -> let name: type = value
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
